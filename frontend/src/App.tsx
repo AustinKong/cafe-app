7c2fc8e35a80cad7c60db32bcd86cb605e134da4
Austin Kong <konghoitec@gmail.com>
2025-11-06 12:51:44 +0800

    chore: setup frontend with antd template

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Typography } from 'antd'
+
+const { Title } = Typography
+
+function App() {
+
+  return (
+    <>
+      <Title>Hello world</Title>
+    </>
+  );
+}
+
+export default App
