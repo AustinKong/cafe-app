6b8ff069f48ce8f9c545223331a852a9b09f96c3
Austin Kong <konghoitec@gmail.com>
2025-11-07 22:05:16 +0800

    feat: dockerization

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
 import errorHandler from "./middleware/errorHandler";
 import logger from "./middleware/logger";
 import cafeRoutes from "./routes/cafeRoutes";
@@ -14,11 +15,16 @@ app.use(express.json());
 app.use(logger);
 
 app.use('/data', express.static('data'));
+app.use(express.static('public'));
 
 app.get("/api/health", (req, res) => res.json({ status: "ok" }));
 
 app.use('/api/cafes', cafeRoutes);
 app.use('/api/employees', employeeRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 app.use(errorHandler);
 
